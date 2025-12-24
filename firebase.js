73363c1bd8e52a11913a67a94f9c989109bcc078
dmvdaveeeee <arefurbish12@gmail.com>
2025-12-24 17:51:08 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,23 +1,17 @@
-// firebase.js (WORKING VERSION FOR DAVETUTORS)
-
-// Import Firebase SDKs (WEB SAFE)
+// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// Your Firebase config (YOUR REAL PROJECT)
 const firebaseConfig = {
   apiKey: "AIzaSyAGeDpvMq3l41NlGeQoXu8c4xrpit9rHsk",
   authDomain: "davetutors-626ad.firebaseapp.com",
   projectId: "davetutors-626ad",
-  storageBucket: "davetutors-626ad.firebasestorage.app",
+  storageBucket: "davetutors-626ad.appspot.com",
   messagingSenderId: "606494183906",
   appId: "1:606494183906:web:052203d3e339830cc5e9bb"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// EXPORT THESE (VERY IMPORTANT)
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
